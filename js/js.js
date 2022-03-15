cec69715c074a2daf3758cafd3e2dfeed3f482ee
rabbitohh <96970233+rabbitohh@users.noreply.github.com>
2022-03-15 21:49:28 +0800

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,24 +1,39 @@
-function $(id)
+function fid(id)
 {
 
 	return document.getElementById(id);
 }
 function ChangeHeight()
 {
-	var a=$("bigdiv").offsetWidth;
-	var b=a/9*7;
-	var c=a/250*3;
-	$("bigdiv").style.height=b+"px"; //设定高
-	$("bigdiv").style.fontSize=c+"px";//设定字体
+	var width=document.documentElement.clientWidth;
+	var height=document.documentElement.clientHeight;
+	var a=fid("bigdiv").offsetWidth;
+	var bl=width/height;
+	var num=$("#bigdiv li").length;
+	var wnum=Math.ceil(bl/0.4);
+	var hnum=Math.ceil(num/wnum);
+	var hperc=1/hnum;
+	var wperc=1/wnum;
+	console.log($("#bigdiv li").length);
+	$("mask1").siblings().css("height",hperc*100+"%");
+	$("mask1").siblings().css("width",wperc*85+"%");
+	$("mask1").siblings().css("margin-left",wperc*7.5+"%");
+	$("mask1").siblings().css("margin-right",wperc*7.5+"%");
+	fid("bigdiv").style.height=((a/wnum)*1.38*hnum)+"px"; //设定高
+	fid("bigdiv").style.fontSize=(a/250*hnum)+"px";//设定字体
 }
+/*$(document).ready(function()
+{
+  $("mask1").siblings().css("height",perc+"%");
+});*/
 window.onresize=window.onload=function()
 {
 	ChangeHeight();
 }
 function disappear()
 {
-	$("loadwait").style.opacity=0;
-	$("loadwait").style.zIndex=-1;
+	fid("loadwait").style.opacity=0;
+	fid("loadwait").style.zIndex=-1;
 }
 document.onreadystatechange=function ()
 {
